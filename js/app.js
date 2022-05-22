61b70848689291153bb654f7b17a24f886e77e48
GKacGit <kacper.gesla@student.pcez.pl>
2022-05-22 14:13:56 +0200

    koncowe pierwiastki

--- js/app.js
+++ js/app.js
@@ -5,36 +5,48 @@ y = 0;
 for (x = 0; x < container.length; x++) {
     if (container[x].textContent != 'DE' && container[x].textContent != 'DEL' && container[x].textContent != '57-71' && container[x].textContent != '89-103') {
         emelentsSymbols[y] = container[x];
+        // wykluczanie niewidzialnych divów i niepotrzebnych kafelków
 
         console.log(y + "-" + emelentsSymbols[y].textContent);
+        // wyświetlanie numeru pierwiastka i jego skrót w konsoli
         y++;
     }
 }
 game();
 
 function game() {
-    console.log(repeatElements);
     if (repeatElements.length >= elements.length) {
         console.log("koniec gry");
+        // wyświetlanie "koniec gry" w konsoli
     } else {
-        losuj = Math.floor(Math.random() * elements.length);
-        if (repeatElements.includes(losuj)) {
+        randomElement = Math.floor(Math.random() * elements.length);
+        // losowanie liczy z tablicy pierwiastków
+        if (repeatElements.includes(randomElement)) {
+            // sprawdza czy wylosowana liczba zawiera sie w tablicy powtórzonych pierwiastków
             game();
         } else {
-            emelentsSymbols[losuj].parentElement.classList.add("checked");
-            repeatElements.push(losuj)
+            emelentsSymbols[randomElement].parentElement.classList.add("checked");
+            // zaznaczanie wylosowanego pierwiastka na różowo
+            repeatElements.push(randomElement)
         }
     }
 }
 
 function checkElement(event) {
     if (event.keyCode == 13) {
-        if (event.target.value == elements[losuj][1]) {
-            emelentsSymbols[losuj].parentElement.classList.remove("checked");
-            emelentsSymbols[losuj].parentElement.classList.add("goodAnswer");
+        // jeżeli enter zostanie kliknięty wokonaj
+        if (event.target.value == elements[randomElement][1]) {
+            // jeżeli pierwiastek został wylosowany
+            emelentsSymbols[randomElement].parentElement.classList.remove("checked");
+            // zaznaczanie wylosowanego pierwiastka na różowo
+            emelentsSymbols[randomElement].parentElement.classList.add("goodAnswer");
+            // zaznaczanie poprawnie nazwanego pierwiastka na zielono
         } else {
-            emelentsSymbols[losuj].parentElement.classList.remove("checked");
-            emelentsSymbols[losuj].parentElement.classList.add("badAnswer");
+            emelentsSymbols[randomElement].parentElement.classList.remove("checked");
+            // zaznaczanie wylosowanego pierwiastka na różowo
+            emelentsSymbols[randomElement].parentElement.classList.add("badAnswer");
+            // zaznaczanie źle nazwanego pierwiastka na czerwono
+
         }
         game();
     }
